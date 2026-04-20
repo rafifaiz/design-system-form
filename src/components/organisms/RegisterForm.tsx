14fa21fa652e9ab7a3c77e28b5d40ffacfb24024
Nama Anda <email@anda.com>
2026-04-20 17:01:22 +0700

    build: successful build with Tailwind CSS

--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Input } from "../atoms/input";
+import { Input } from "../atoms/Input";
 import { PasswordInput } from "../atoms/PasswordInput";
 import { Select } from "../atoms/Select";
 import { Textarea } from "../atoms/Textarea";
